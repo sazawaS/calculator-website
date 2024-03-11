af5121cabf67ec11031608121570b8669c0cf1e2
sazawa <131285392+sazawaS@users.noreply.github.com>
2024-03-11 11:31:00 +0545

    "added" a little bit of input handler while inputting equtions.
    its so bad i only accounted for x=y equation

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
+const vars = ["x", "y"];
 let output = "";
 
 
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-   
+
     output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "graph") {
+    const equation = output;
+    let tempValX = 0;
+    let tempValY = 0;
+    for (let i = -5; i <= 5; i++) {
+      for (let j = -5; j <= 5; j++) {
+        tempValX = ((equation.replace("x", i)).replace("==", "")).replace("y", "");
+        tempValY = ((equation.replace("y", j)).replace("==", "")).replace("x", "");
+        console.log(`Value for x=${tempValX} and y=${tempValY}`);
+      }
+    }
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    
+
     output = output.toString().slice(0, -1);
   } else {
-    
+
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
@@ -24,5 +36,5 @@ const calculate = (btnValue) => {
 
 
 buttons.forEach((button) => {
- button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
